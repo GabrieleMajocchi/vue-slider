bca9994337bfc8dc4db624ccc079f19c82a1697b
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-05 16:33:05 +0200

    updated myscript so next and previous buttons works

--- js/myscript.js
+++ js/myscript.js
@@ -25,114 +25,19 @@ createApp({
         }
     },
     methods: {
+        previous(){
+            if ( this.activeIndex === 0){
+                this.activeIndex = this.images.length - 1;
+            } else {
+                this.activeIndex--;
+            }
+        },
+        next(){
+            if ( this.activeIndex === this.images.length - 1){
+                this.activeIndex = 0;
+            } else {
+                this.activeIndex++;
+            }
+        },
     }
 }).mount('#app');
-
-
-
-
-const carouselGroup = document.querySelector('div.carousel');
-let activeIndex = 0;
-let i = 0;
-
-// ---Adding various slide with elements---
-images.forEach(element => {
-    carouselGroup.innerHTML +=
-    `<div class="carousel-item ${i}">
-        <img src="./${element.image}" alt="Carousel slide image">
-        <div>
-            <h4>${element.title}</h4>
-            <p>${element.text}</p>
-        </div>
-    </div>`;
-    i++;
-});
-
-// ---Adding active based on current slide---
-addactive(activeIndex);
-
-// ---Adding function to go to the previous slide---
-const prevButton = document.querySelector('div.previous-button');
-prevButton.addEventListener('click', previous);
-function previous(){
-    removeactive();
-    if (activeIndex == 0 ) {
-        activeIndex = images.length - 1;
-    } else {
-        activeIndex = activeIndex - 1;
-    }
-    addactive(activeIndex);
-};
-
-// ---Adding function to go to the next slide---
-const nextButton = document.querySelector('div.next-button');
-nextButton.addEventListener('click', next);
-function next(){
-    removeactive();
-    if (activeIndex == images.length - 1 ) {
-        activeIndex = 0;
-    } else {
-        activeIndex = activeIndex + 1;
-    }
-    addactive(activeIndex);
-};
-
-// ---Adding function to select slide clicking dots---
-for(let b=0 ; b<images.length ;b++){
-const dot = document.querySelector('#dot'+b);
-dot.addEventListener('click', function(){
-    removeactive();
-    addactive(b);
-    activeIndex = b;
-})};
-
-// ---Adding timed function---
-let timer = setInterval (next, 3000);
-
-// ---Adding stop to timed function---
-const stop = document.querySelector('.fa-stop');
-stop.addEventListener('click', function(){
-    clearInterval(timer);
-});
-
-// ---Adding play to timed function---
-const play = document.querySelector('.fa-play');
-play.addEventListener('click', function(){
-    clearInterval(timer);
-    timer = setInterval (next, 3000);
-});
-
-// ---Adding reverse timed function---
-let rev = true;
-const reverse = document.querySelector('.fa-rotate-left');
-reverse.addEventListener('click', function(){
-    if (rev === true){
-        clearInterval(timer);
-        timer = setInterval (previous, 3000);
-        rev = false;
-    }else{
-        clearInterval(timer);
-        timer = setInterval (next, 3000);
-    }
-});
-
-// --------------
-// ---Function---
-// --------------
-
-/**
- * Activate the selected index in the slider
- * @param {number} index number of current index to activate
- */
-function addactive(index){
-    document.querySelectorAll('div.carousel-item')[index].classList.add('active');
-    document.querySelectorAll('div.dots')[index].classList.add('active');
-}
-
-/**
- * Remove active state from the slider
- */
-function removeactive(){
-    document.querySelector('div.carousel-item.active').classList.remove('active');
-    document.querySelector('div.dots.active').classList.remove('active');
-}
