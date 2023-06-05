4213ac6a54b84a4ac7b2e7edb412babbd3ff8d4f
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-05 17:12:32 +0200

    added funtions to play, stop and reverse the autoplay of the slides

--- js/myscript.js
+++ js/myscript.js
@@ -22,6 +22,8 @@ createApp({
                 text: "Il Monte Fuji è un'icona nazionale del Giappone e uno dei vulcani più famosi al mondo. Situato a circa 100 chilometri a sud-ovest di Tokyo, il Fuji si innalza fino a un'altitudine di 3.776 metri. La sua forma conica perfetta, le foreste rigogliose e i laghi pittoreschi attirano visitatori e artisti da secoli. È anche una meta molto popolare per gli escursionisti che vogliono raggiungere la sua cima e ammirare l'alba spettacolare.",}
         ],
             activeIndex : 0,
+            timer: setInterval (this.next, 1000),
+            rev: true,
         }
     },
     methods: {
@@ -41,37 +43,23 @@ createApp({
         },
         activateImage(imageindex){
             this.activeIndex = imageindex;
-        }
-    }, 
-    mounted() {
-        let timer = setInterval (this.next, 3000);
+        },
+        stopAutoplay(){
+            clearInterval(this.timer);
+        },
+        playAutoplay(){
+            clearInterval(this.timer);
+            this.timer = setInterval (this.next, 1000);
+        },
+        reverseAutoplay(){
+            if (this.rev === true){
+                clearInterval(this.timer);
+                this.timer = setInterval (this.previous, 1000);
+                this.rev = false;
+            }else{
+                clearInterval(this.timer);
+                this.timer = setInterval (this.next, 1000);
+            }
+        },
     }
 }).mount('#app');
-
-
-// // ---Adding stop to timed function---
-// const stop = document.querySelector('.fa-stop');
-// stop.addEventListener('click', function(){
-//     clearInterval(timer);
-// });
-
-// // ---Adding play to timed function---
-// const play = document.querySelector('.fa-play');
-// play.addEventListener('click', function(){
-//     clearInterval(timer);
-//     timer = setInterval (next, 3000);
-// });
-
-// // ---Adding reverse timed function---
-// let rev = true;
-// const reverse = document.querySelector('.fa-rotate-left');
-// reverse.addEventListener('click', function(){
-//     if (rev === true){
-//         clearInterval(timer);
-//         timer = setInterval (previous, 3000);
-//         rev = false;
-//     }else{
-//         clearInterval(timer);
-//         timer = setInterval (next, 3000);
-//     }
-// });
